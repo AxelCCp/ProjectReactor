5628a70441a42c9d807fab3fb88de22281cda577
Boo <hpmajin@outlook.es>
2021-10-08 22:07:27 -0500

    Hasta video 5, cosas básicas :|

---
+++ demo-reactor/src/main/java/com/reactor/model/Persona.java
@@ -0,0 +1,42 @@
+package com.reactor.model;
+
+public class Persona {
+	
+	public Persona(Integer idPersona, String nombre, Integer edad) {
+		this.idPersona = idPersona;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	public Integer getIdPersona() {
+		return idPersona;
+	}
+	public void setIdPersona(Integer idPersona) {
+		this.idPersona = idPersona;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Persona [idPersona=" + idPersona + ", nombre=" + nombre + ", edad=" + edad + "]";
+	}
+
+
+
+
+	private Integer idPersona;
+	private String nombre;
+	private Integer edad;
+
+}
